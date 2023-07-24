673bf9861e72a4a1edb57dc0164b6a93dbf0ec4e
shayan kolahi <shayankolahi30@gmail.com>
2023-07-24 20:33:39 +0330

    make CanActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AuthGuard} from "./guards/auth.guard";
 
 const routes: Routes = [
-  {path: "auth",loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)}
+
+  {path: "auth",loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) , canActivate:[AuthGuard]}
 
 ];
 
