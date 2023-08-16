d8d4e4ad9ceccdd0360563029169b9592edebb2b
shayan <shayankolahi30@gmail.com>
2023-08-16 00:32:14 -0700

    admin

--- src/app/modules/admin/components/sidebar/sidebar.component.ts
+++ src/app/modules/admin/components/sidebar/sidebar.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
   constructor() { }
-
   ngOnInit(): void {
   }
-
+  items: any[] = [
+    {
+      name: "داشبورد",
+      link: "/admin/dashboard",
+      img: 'crate-apple.svg'
+    },
+    {
+      name: "لیست کاربران",
+      link: "/admin/asd",
+      img: 'user.svg'
+    },
+  ]
 }
